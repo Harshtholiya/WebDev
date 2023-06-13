41e18c7fb3332021cf6f33878ac26a4dbf26700e
Harshtholiya <harshpateltholiya@gmail.com>
2023-06-13 14:21:02 +0100

    Update

---
+++ mod_6/js/script.js
@@ -0,0 +1,10 @@
+
+function sayHello() {
+	var name =
+	document.getElementById("name").value;
+	var message = "Hello " + name + "!";
+
+	document
+	.getElementById("content")
+	.textContent = message;
+}
